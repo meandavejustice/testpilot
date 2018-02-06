ce31de5807b2d0238485d84269bc265b1377b219
dave justice <djustice@mozilla.com>
2018-02-06 17:13:05 -0500

    Remove unused survey popup from addon, remove bootstrap and sdk ... examples from docs

--- addon/src/lib/webExtension.js
+++ addon/src/lib/webExtension.js
@@ -2,15 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* global Components, Services, LegacyExtensionsUtils */
+/* global Components, LegacyExtensionsUtils */
 
 import PubSub from "pubsub-js";
 import allTopics from "./topics";
 
-import { log, addonMetadata } from "./utils";
+import { log } from "./utils";
 
 const { utils: Cu } = Components;
-Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/LegacyExtensionsUtils.jsm");
 
 const bootstrapTopics = (...args) => allTopics("bootstrap", ...args);
@@ -48,12 +47,6 @@ export async function startupWebExtension(data, reason) {
     }
   );
 
-  // TODO: Move this to a different module?
-  registerWebExtensionAPI("getAddonMetadata", () => addonMetadata);
-
-  // TODO: Move this to a different UI-centric module?
-  registerWebExtensionAPI("clickBrowserAction", () => clickBrowserAction());
-
   // Return a promise that resolves when the WebExtension signals ready
   return new Promise(resolve => {
     const token = PubSub.subscribe(webExtensionAPITopics("ready"), () => {
@@ -84,13 +77,4 @@ export function registerWebExtensionAPI(op, handler) {
 
 export function sendToWebextension(op, message) {
   ports.forEach(port => port.postMessage({ op, message }));
-}
-
-// TODO: Remove this? Experiment in summoning the pop-up for surveys
-const BROWSER_ACTION_BUTTON_ID = "_testpilot-addon-browser-action";
-function clickBrowserAction(sendReply) {
-  const win = Services.wm.getMostRecentWindow("navigator:browser");
-  const button = win.document.getElementById(BROWSER_ACTION_BUTTON_ID);
-  if (button) button.click();
-  return sendReply();
 }
